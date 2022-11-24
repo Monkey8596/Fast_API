ebd33bd00f42fe7839880b197874844b610628f0
Sayin <luffyp2001@gmail.com>
2022-11-23 22:25:12 -0500

    validations_body

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ app = FastAPI()
 
 # Models
 
+class Location(BaseModel):
+    city : str
+    state : str
+    country: str
+
 class Person(BaseModel):
     first_name : str
     last_name : str
@@ -71,3 +76,23 @@ def show_person(
 
 ):
     return{person_id: 'It exists!'}
+
+
+
+# Validations : Request Body 
+
+@app.put('/person/{person_id}')
+def update_person(
+    person_id : int = Path(
+        ...,
+        title = 'Person ID',
+        description = 'This is the person ID',
+        gt = 0 
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    results = person.dict()
+    results.update(location.dict())
+    return results
+
