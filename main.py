b00ce492693f9bb5024ad788734d2c0adc8f4470
Sayin <luffyp2001@gmail.com>
2022-11-28 15:49:05 -0500

    cookies_and_headers

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pydantic import Field, EmailStr, PaymentCardNumber
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 from fastapi import status
 
 app = FastAPI()
@@ -189,6 +189,10 @@ def update_person(
     results.update(location.dict())
     return results
 
+
+
+#Forms
+
 @app.post(
     path='/login',
     response_model=LoginOut,
@@ -196,5 +200,34 @@ def update_person(
 )
 def login(username:str = Form(...), password: str = Form(...) ):
     return LoginOut(username=username)
+
+
+
+# Cookies and headers Parameters
+
+@app.post(
+    path='/contact',
+    status_code=status.HTTP_200_OK,
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1,
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1,
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length=20, 
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None),
+):
+    return user_agent
  
 
